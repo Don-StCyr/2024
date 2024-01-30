793982f693dc2341e5c0a73170fa8adc7bf7bad0
Don St. Cyr <40702838+Don-StCyr@users.noreply.github.com>
2024-01-29 20:34:10 -0600

    Change 3

--- 1st_File.ps1
+++ 1st_File.ps1
@@ -1,1 +1,5 @@
-get=childitem C:\Users\Don\Documents\Git\20240129
+get-childitem C:\Users\Don\Documents\Git\20240129
+
+Get-ChildItem C:\Users\Don\Documents\Git -Recurse
+
+Get-ChildItem C:\Users\Don\Documents -Recurse
