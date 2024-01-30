fe8b3c132afebab2976256200467712138eefc3e
Don St. Cyr <40702838+Don-StCyr@users.noreply.github.com>
2024-01-29 21:04:38 -0600

    "My last changes"

--- 1st_File.ps1
+++ 1st_File.ps1
@@ -3,3 +3,5 @@ get-childitem C:\Users\Don\Documents\Git\20240129
 Get-ChildItem C:\Users\Don\Documents\Git -Recurse
 
 Get-ChildItem C:\Users\Don\Documents -Recurse
+
+Get-ChildItem C:\Users\Don\Documents -Recurse -Filter *.txt
